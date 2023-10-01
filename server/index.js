2beafa486c14451f29e5b63f2a1a9f2eb40d43e6
rajatm17 <93270086+rajatm17@users.noreply.github.com>
2023-10-01 08:33:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,13 +9,20 @@ require('dotenv').config();
 // import userRouter from './router/userRouter';
 // import todolistRouter from './router/todolistRouter';
 
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+       next();
+ });
+
 const userRouter = require('./router/userRouter');
 const todolistRouter = require('./router/todolistRouter');
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log('connection established');
 });
 
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
